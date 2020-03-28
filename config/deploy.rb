120abdab7f449fb0465cccca4e4a40b04fe9d0fe
林晃人 <hayashi@hayashiakiraninnoMacBook-Pro.local>
2020-03-28 11:26:32 +0900

    up

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,10 @@ lock "~> 3.12.1"
 
 set :application, "portfolio-zuborasann"
 set :repo_url, "git@github.com:asatuki/portfolio-zuborasann.git"
+set :deploy_to, "/home/ec2-user/portfolio-zuborasann"
+set :rbenv_ruby, '2.5.7'
+set :linked_files, %w{config/master.key .env}
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
